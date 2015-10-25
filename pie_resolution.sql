8751a4b114757673fff5c9dc855a08111eba18f7
withchange <1663545852@qq.com>
2015-10-25 20:46:19 +0800

    Create pie_resolution.sql

---
+++ pie_resolution.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `pie_resolution` (
+`type`  varchar(30) NULL ,
+`num`  int NULL 
+)
+;
+insert into pie_resolution VALUES('FIXED',392910);
+insert into pie_resolution VALUES('WORKSFORME',	169870);
+insert into pie_resolution VALUES('WONTFIX',25524);
+insert into pie_resolution VALUES('LATER',910);
