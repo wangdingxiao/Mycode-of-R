be8eec2e5b001b6e07b233094c1835eb43ec6654
withchange <1663545852@qq.com>
2015-10-28 22:15:54 +0800

    Update bug_life.sql

--- bug_life.sql
+++ bug_life.sql
@@ -1,6 +1,6 @@
 create table mozilla_bug_status SELECT Bug_ID,FROM_UNIXTIME(`When`,'%Y-%m-%d') as time ,Removed,Added FROM `2011_mozilla_activity_level2` where what='Status';
 create table bug_status_life SELECT Bug_ID, MIN(time) as start ,MAX(time) as end from mozilla_bug_status where Removed in ('NEW','REOPENED') or Added in ('VERIFIED','CLOSED') GROUP BY Bug_ID;
-create bug_last select Bug_ID ,DATEDIFF(`end`,`start`) as day from bug_status_life ORDER BY day desc;
+create table bug_last select Bug_ID ,DATEDIFF(`end`,`start`) as day from bug_status_life ORDER BY day desc;
 CREATE TABLE `dayrange` (
 `range`  varchar(20) NULL ,
 `num`  int NULL 
